b6d4fcf6103a87f132c4d1fb92fe8b6627df8a44
max <max.bispham@gmail.com>
2026-03-16 03:35:28 -0400

    not tested but added probe function full

--- sht30_driver/sht30.c
+++ sht30_driver/sht30.c
@@ -2,12 +2,26 @@
 #include <linux/init.h>
 #include <linux/fs.h>
 #include <linux/i2c.h>
+#include <linux/slab.h>
+#include <linux/device.h>
 
 
+
+// initializes major to negative
 static int major = -1;
 
-static struct i2c_client *global_client;
+// This struct holds the kernel's client pointer
+struct sht30_data {
+	struct i2c_client *client;
+};
 
+static struct sht30_data *global_data;
+
+// Device class struct
+static struct class *sht30_class;
+
+// Device device struct
+static struct device *sht30_device;
 
 // Character Driver
 
@@ -26,7 +40,7 @@ static ssize_t sht30_write(struct file *f, const char __user *u, size_t s, loff_
 
 static int sht30_open(struct inode *n, struct file *f)
 {
-	f->private_data = global_client;
+	f->private_data = global_data;
 	printk("sht30_cdev - Open is called\n");
 	return 0;
 }
@@ -64,17 +78,69 @@ static const struct of_device_id sht30_of_match[] = {
 };
 
 
+
+
 MODULE_DEVICE_TABLE(i2c, sht30_idtable);
 
 static int sht30_probe(struct i2c_client *client)
 {
+
+	u8 cmd[2] = {0xF3, 0x2D};
+	u8 status[3];
+	int ret;
+
+	struct sht30_data *data;
+
+
 	printk("sht30 - probe called\n");
-	global_client = client;
+
+	// Check to make sure device is there
+	ret = i2c_master_send(client, cmd, 2);
+	if (ret < 0)
+	{
+		printk("sht30 - device not found\n");
+		return ret;
+	}
+
+	ret = i2c_master_recv(client, status, 3);
+	if (ret < 0)
+	{
+		printk("sht30 - failed to read status\n");
+		return ret;
+	}
+
+	// Allocating private data
+
+	data = devm_kzalloc(&client->dev, sizeof(*data), GFP_KERNEL);
+	if (!data)
+	{
+		return -ENOMEM;
+	}
+	data->client = client;
+	i2c_set_clientdata(client, data);
+	global_data = data;
 
 	major = register_chrdev(0, "sht30", &fops);
 	if (major < 0) {
 		printk("sht30 - failed to register char device\n");
 		return major;
+	}
+
+	// Creating a class
+        sht30_class = class_create("sht30");
+        if (IS_ERR(sht30_class))
+        {       
+        	unregister_chrdev(major, "sht30");
+		return PTR_ERR(sht30_class);
+        }
+
+	// Creating a device
+	sht30_device = device_create(sht30_class, NULL, MKDEV(major, 0), NULL, "sht30");
+	if (IS_ERR(sht30_device))
+	{
+		class_destroy(sht30_class);
+		unregister_chrdev(major, "sht30");
+		return PTR_ERR(sht30_device);
 	}
 
 	return 0;
@@ -84,8 +150,13 @@ static int sht30_probe(struct i2c_client *client)
 static void sht30_remove(struct i2c_client *client)
 {
 	printk("sht30 - remove called\n");
-	if (major >= 0) {
-		unregister_chrdev(major, "sht30");	
+
+	device_destroy(sht30_class, MKDEV(major, 0));
+	class_destroy(sht30_class);
+	
+	if (major >= 0)
+	{
+		unregister_chrdev(major, "sht30");
 	}
 }
 
